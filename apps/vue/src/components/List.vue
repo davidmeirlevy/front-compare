8c760d4336611e83efc3cc949be027790e91a583
David Meir-Levy <davidmeirlevy@gmail.com>
2021-12-30 14:35:52 +0200

    reset infinte scroll when list changes

--- apps/vue/src/components/List.vue
+++ apps/vue/src/components/List.vue
@@ -5,14 +5,16 @@
 </template>
 
 <script setup>
-import { computed, onUnmounted, ref } from 'vue';
+import { computed, onUnmounted, ref, watch } from 'vue';
 import Item from './Item.vue';
 
 const props = defineProps({
 	items: Array
 })
 
+
 const length = ref(100);
+watch(() => props.items, () => length.value = 100)
 
 const shownItems = computed(() => props.items.slice(0, length.value));
 
